73c1c3777deebdaab1c54c6a5202644a89602af0
Zoe Mendez <zoemendez316@gmail.com>
2022-09-16 14:19:14 -0700

    Done with backend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,44 @@ post "/games" do
 games.to_json()
 end
 
+get "/games/:id" do
+  games = Game.find(params[:id])
+  games.to_json(include: :reviews)
+end
+
+get  "/users" do
+  users = User.all
+  users.to_json()
+end
+
+get "/users/:id" do
+users = User.find(params[:id])
+users.to_json()
+end
+
+post "/reviews" do
+reviews = Review.create(
+  review: params[:review],
+  game_id: params[:game_id],
+  user_id: params[:user_id],
+  rating: params[:rating])
+
+reviews.to_json
+end
+
+delete "/reviews/:id" do
+  reviews = Review.find(params[:id])
+reviews.destroy
+reviews.to_json
+end
+
+patch "/reviews/:id" do 
+  reviews = Review.find(params[:id])
+  reviews.update(:review => params[:review])
+  reviews.to_json
+end
+
+
+
+
 end
